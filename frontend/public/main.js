db40db937b10bbcd1125c5f81130374dd33df27b
Lucas de Lima Bica <delimabica@gmail.com>
2025-11-17 16:38:00 +0000

    chore: Pipedrive SDK active

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,26 +1,26 @@
 // =======================
 // PIPEDRIVE SDK SETUP
 // =======================
-//const AppExtensionsSDK = window.AppExtensionsSDK;
-//let sdk = null;
-//let loggedUser = null;
-//
-//async function initPipedriveSDK() {
-//    try {
-//        sdk = await new AppExtensionsSDK().initialize();
-//        // Define o tamanho inicial do iframe
-//        await sdk.execute(AppExtensionsSDK.Command.RESIZE, {
-//            width: 500,
-//            height: 450
-//        });
-//        loggedUser = await sdk.user.getUserInfo();
-//        console.log("SDK Loaded. Logged user:", loggedUser);
-//    } catch (error) {
-//        console.error("Error at the Pipedrive SDK:", error);
-//        // Aqui é normal falhar se estiveres a testar fora do Pipedrive.
-//    }
-//}
-//initPipedriveSDK();
+const AppExtensionsSDK = window.AppExtensionsSDK;
+let sdk = null;
+let loggedUser = null;
+
+async function initPipedriveSDK() {
+    try {
+        sdk = await new AppExtensionsSDK().initialize();
+        // Define o tamanho inicial do iframe
+        await sdk.execute(AppExtensionsSDK.Command.RESIZE, {
+            width: 500,
+            height: 450
+        });
+        loggedUser = await sdk.user.getUserInfo();
+        console.log("SDK Loaded. Logged user:", loggedUser);
+    } catch (error) {
+        console.error("Error at the Pipedrive SDK:", error);
+        // Aqui é normal falhar se estiveres a testar fora do Pipedrive.
+    }
+}
+initPipedriveSDK();
 
 // =======================
 // APP LOGIC
